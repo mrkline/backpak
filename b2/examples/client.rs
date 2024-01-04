14c45eed1342609a8ae110cea7ba03270b92cfe6
Matt Kline <matt@bitbashing.io>
2024-01-04 01:15:35 -0800

    WIP: B2 backend
    We don't need a big fancy async runtime to make a handful of HTTP
    requests, nor fancy crate for a few REST endpoints.

---
+++ b2/examples/client.rs
@@ -0,0 +1,99 @@
+// Smoke test of gets and puts we need for B2 usage.
+
+use anyhow::{Context, Result};
+use camino::{Utf8Path, Utf8PathBuf};
+use clap::{ArgAction, Parser, Subcommand};
+use serde_derive::{Deserialize, Serialize};
+use simplelog::*;
+
+#[derive(Debug, Parser)]
+struct Args {
+    /// Verbosity (-v, -vv, -vvv, etc.)
+    #[clap(short, long, action(ArgAction::Count))]
+    verbose: u8,
+    /// Prepend ISO-8601 timestamps to all trace messages (from --verbose).
+    /// Useful for benchmarking.
+    #[clap(short, long, verbatim_doc_comment)]
+    timestamps: bool,
+
+    #[clap(subcommand)]
+    subcommand: Command,
+
+    #[clap(short, long)]
+    credentials: Utf8PathBuf,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Credentials {
+    pub key_id: String,
+    pub application_key: String,
+}
+
+fn read_creds(p: &Utf8Path) -> Result<Credentials> {
+    let s = std::fs::read_to_string(p).with_context(|| format!("Couldn't read creds from {p}"))?;
+    let c = toml::from_str(&s)?;
+    Ok(c)
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    List,
+    Get,
+    Put,
+}
+
+fn main() {
+    run().unwrap_or_else(|e| {
+        log::error!("{:?}", e);
+        std::process::exit(1);
+    });
+}
+
+fn run() -> Result<()> {
+    let args = Args::parse();
+    init_logger(&args);
+
+    let creds = read_creds(&args.credentials)?;
+
+    b2::Session::new(&creds.key_id, &creds.application_key)?;
+    Ok(())
+}
+
+// Copied from the backpak main:
+
+/// Set up simplelog to spit messages to stderr.
+fn init_logger(args: &Args) {
+    let mut builder = ConfigBuilder::new();
+    builder.set_target_level(LevelFilter::Off);
+    builder.set_thread_level(LevelFilter::Off);
+    if args.timestamps {
+        builder.set_time_format_rfc3339();
+        builder.set_time_level(LevelFilter::Error);
+    } else {
+        builder.set_time_level(LevelFilter::Off);
+    }
+
+    let level = match args.verbose {
+        0 => LevelFilter::Warn,
+        1 => LevelFilter::Info,
+        2 => LevelFilter::Debug,
+        _ => LevelFilter::Trace,
+    };
+
+    if level == LevelFilter::Trace {
+        builder.set_location_level(LevelFilter::Error);
+    }
+    builder.set_level_padding(LevelPadding::Left);
+
+    let config = builder.build();
+
+    TermLogger::init(
+        level,
+        config.clone(),
+        TerminalMode::Stderr,
+        ColorChoice::Auto,
+    )
+    .or_else(|_| SimpleLogger::init(level, config))
+    .context("Couldn't init logger")
+    .unwrap()
+}
