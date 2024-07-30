e0f5319e9c12ea750eaeae9fd8e84a4b99c348e1
Matt Kline <matt@bitbashing.io>
2024-07-29 23:13:10 -0700

    Add file version numbers.
    I put off doing so for ages, but let's not tempt fate.

--- src/file_util.rs
+++ src/file_util.rs
@@ -12,7 +12,8 @@ use crate::counters;
 
 /// Checks for the given magic bytes at the start of the file
 pub fn check_magic<R: Read>(r: &mut R, expected: &[u8]) -> Result<()> {
-    let mut magic: [u8; 8] = [0; 8];
+    assert_eq!(expected.len(), 9); // Magic + version
+    let mut magic: [u8; 9] = [0; 9];
     r.read_exact(&mut magic)?;
     ensure!(
         magic == expected,
