d1947814ee78cf744b12917517ba6762472726cf
Matt Kline <matt@bitbashing.io>
2024-09-12 00:09:21 -0700

    copy: Don't break all trees on filter
    Filtering a tree means its ID changes. Use the Merkle, Luke.

--- src/repack.rs
+++ src/repack.rs
@@ -1,20 +1,21 @@
 //! Shared utilities to repack blobs,
 //! either loose ones in `backpak prune` or to another repo in `backpak copy`
-use anyhow::{anyhow, ensure, Context, Result};
+use anyhow::{anyhow, Context, Result};
 use camino::Utf8Path;
 use rustc_hash::FxHashSet;
 use tracing::*;
 
-use crate::{backup, blob, hashing::ObjectId, read, snapshot, tree};
+use crate::{backup, blob, hashing::ObjectId, read, snapshot::Snapshot, tree};
 
 pub struct SnapshotAndForest {
     pub id: ObjectId,
-    pub snapshot: snapshot::Snapshot,
+    pub snapshot: Snapshot,
     pub forest: tree::Forest,
 }
 
+/// Load all trees for each given snapshot
 pub fn load_forests(
-    snapshots: Vec<(snapshot::Snapshot, ObjectId)>,
+    snapshots: Vec<(Snapshot, ObjectId)>,
     tree_cache: &mut tree::Cache,
 ) -> Result<Vec<SnapshotAndForest>> {
     snapshots
@@ -37,6 +38,11 @@ pub enum Op {
     Prune,
 }
 
+/// Walk each snapshot and its forest, copying as-needed to the given backup.
+///
+/// This returns a new list of snapshots since filtering a tree changes its contents,
+/// which changes its ID, which changes the IDs of all trees above it.
+/// Aren't Merkle trees fun?
 pub fn walk_snapshots<Filter>(
     op: Op,
     snapshots_and_forests: &[SnapshotAndForest],
@@ -44,19 +50,21 @@ pub fn walk_snapshots<Filter>(
     reader: &mut read::ChunkReader,
     packed_blobs: &mut FxHashSet<ObjectId>,
     backup: &mut Option<backup::Backup>,
-) -> Result<()>
+) -> Result<Vec<Snapshot>>
 where
     Filter: FnMut(
         &Utf8Path,
         // More some day?
     ) -> Result<bool>,
 {
-    for snapshot in snapshots_and_forests.iter() {
-        walk_snapshot(op, snapshot, &mut filter, reader, packed_blobs, backup)?
-    }
-    Ok(())
+    let new_snaps = snapshots_and_forests
+        .iter()
+        .map(|snf| walk_snapshot(op, snf, &mut filter, reader, packed_blobs, backup))
+        .collect::<Result<Vec<_>>>()?;
+    Ok(new_snaps)
 }
 
+/// Walk the given snapshot, copying to the given backup. Return the new (filtered) snapshot.
 fn walk_snapshot<Filter>(
     op: Op,
     snapshot_and_forest: &SnapshotAndForest,
@@ -64,7 +72,7 @@ fn walk_snapshot<Filter>(
     reader: &mut read::ChunkReader,
     packed_blobs: &mut FxHashSet<ObjectId>,
     backup: &mut Option<backup::Backup>,
-) -> Result<()>
+) -> Result<Snapshot>
 where
     Filter: FnMut(
         &Utf8Path,
@@ -76,7 +84,7 @@ where
         Op::Prune => "Repacking loose blobs from snapshot",
     };
     info!("{action} {}", snapshot_and_forest.id);
-    walk_tree(
+    let new_root = walk_tree(
         op,
         filter,
         Utf8Path::new(""),
@@ -86,7 +94,11 @@ where
         packed_blobs,
         backup,
     )
-    .with_context(|| format!("In snapshot {}", snapshot_and_forest.id))
+    .with_context(|| format!("In snapshot {}", snapshot_and_forest.id))?;
+
+    let mut new_snapshot = snapshot_and_forest.snapshot.clone();
+    new_snapshot.tree = new_root;
+    Ok(new_snapshot)
 }
 
 #[expect(clippy::too_many_arguments)] // We know, sit down.
@@ -99,7 +111,7 @@ fn walk_tree<Filter>(
     reader: &mut read::ChunkReader,
     packed_blobs: &mut FxHashSet<ObjectId>,
     backup: &mut Option<backup::Backup>,
-) -> Result<()>
+) -> Result<ObjectId>
 where
     Filter: FnMut(
         &Utf8Path,
@@ -111,6 +123,12 @@ where
         .ok_or_else(|| anyhow!("Missing tree {}", tree_id))
         .unwrap();
 
+    // Cloning the tree node-by-node is wasteful if we didn't actually filter anything out
+    // (which means this tree hasn't changed at all!).
+    // But this is fast enough compared to all the IO that we don't need a separate
+    // "we filtered nothing" path.
+    let mut new_tree = tree::Tree::default();
+
     for (path, node) in tree {
         let mut node_path = tree_path.to_owned();
         node_path.push(path);
@@ -119,7 +137,7 @@ where
             continue;
         }
 
-        match &node.contents {
+        let new_node: tree::Node = match &node.contents {
             tree::NodeContents::File { chunks } => {
                 let mut chunks_repacked = false;
                 let verb = match op {
@@ -138,12 +156,15 @@ where
                 } else {
                     info!("  {:>9} {node_path}", "deduped"); // Sorta; "unneeded"? Bleh.
                 }
+                // We're not changing any files, the node stays the same.
+                node.clone()
             }
             tree::NodeContents::Symlink { .. } => {
-                // Nothing to repack for symlinks.
+                // Nothing to change or repack for symlinks.
+                node.clone()
             }
             tree::NodeContents::Directory { subtree } => {
-                walk_tree(
+                let new_tree = walk_tree(
                     op,
                     filter,
                     &node_path,
@@ -158,14 +179,31 @@ where
                     "finished",
                     std::path::MAIN_SEPARATOR
                 );
+                tree::Node {
+                    contents: tree::NodeContents::Directory { subtree: new_tree },
+                    metadata: node.metadata.clone(),
+                }
             }
-        }
+        };
+        assert!(new_tree.insert(path.clone(), new_node).is_none());
     }
 
-    if packed_blobs.insert(*tree_id) {
-        repack_tree(tree_id, tree, backup)?;
+    // We might have a new tree, we might have the exact same tree.
+    // Serialize and hash it to find out.
+    // (Again, we could have a separate "we didn't filter anything" path,
+    // but it doesn't seem worth it at the moment.)
+    let (serialized, new_tree_id) = tree::serialize_and_hash(&new_tree)?;
+    // If we don't have this tree, new or old, in the backup, add it.
+    if packed_blobs.insert(new_tree_id) {
+        if let Some(b) = backup {
+            b.tree_tx.send(blob::Blob {
+                contents: blob::Contents::Buffer(serialized),
+                id: new_tree_id,
+                kind: blob::Type::Tree,
+            })?;
+        }
     }
-    Ok(())
+    Ok(new_tree_id)
 }
 
 fn repack_chunk<'a, 'b: 'a>(
@@ -182,31 +220,5 @@ fn repack_chunk<'a, 'b: 'a>(
             kind: blob::Type::Chunk,
         })?;
     }
-    Ok(())
-}
-
-fn repack_tree(
-    id: &ObjectId,
-    tree: &tree::Tree,
-    backup: &mut Option<backup::Backup>,
-) -> Result<()> {
-    let (reserialized, check_id) = tree::serialize_and_hash(tree)?;
-    // Sanity check:
-    ensure!(
-        check_id == *id,
-        "Tree {} has a different ID ({}) when reserialized",
-        id,
-        check_id
-    );
-
-    if let Some(b) = backup {
-        let contents = blob::Contents::Buffer(reserialized);
-        b.tree_tx.send(blob::Blob {
-            contents,
-            id: *id,
-            kind: blob::Type::Tree,
-        })?;
-    }
-
     Ok(())
 }
--- src/ui/copy.rs
+++ src/ui/copy.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 
-use anyhow::{ensure, Result};
+use anyhow::Result;
 use camino::{Utf8Path, Utf8PathBuf};
 use clap::Parser;
 
@@ -107,7 +107,7 @@ pub fn run(repository: &Utf8Path, args: Args) -> Result<()> {
     let mf = filter::skip_matching_paths(&args.skips)?;
     let filter = |p: &Utf8Path| Ok(mf(p));
 
-    repack::walk_snapshots(
+    let new_snapshots = repack::walk_snapshots(
         repack::Op::Copy,
         &src_snapshots_and_forests,
         filter,
@@ -122,13 +122,8 @@ pub fn run(repository: &Utf8Path, args: Args) -> Result<()> {
     let _stats = backup.map(|b| b.join()).transpose()?;
 
     if !args.dry_run {
-        for sf in &src_snapshots_and_forests {
-            let new_id = snapshot::upload(&sf.snapshot, &dst_cached_backend)?;
-            ensure!(
-                new_id == sf.id,
-                "Snapshot {} has a different ID ({new_id}) when reserialized",
-                sf.id
-            );
+        for snap in &new_snapshots {
+            snapshot::upload(snap, &dst_cached_backend)?;
         }
     }
 
