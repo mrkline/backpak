419a9cd01cd98a6760d67c0d9de6c8194bb08010
Matt Kline <matt@bitbashing.io>
2024-09-12 00:09:21 -0700

    copy: Don't break all trees on filter
    Filtering a tree means its ID changes. Use the Merkle, Luke.

--- tests/copy.rs
+++ tests/copy.rs
@@ -85,3 +85,74 @@ fn copy_smoke() -> Result<()> {
     // std::mem::forget(copy_dir);
     Ok(())
 }
+
+#[test]
+fn copy_filtered() -> Result<()> {
+    let project_dir = std::env::current_dir()?;
+
+    let backup_dir = tempdir()?;
+    let backup_path = backup_dir.path();
+
+    let working_dir = tempdir()?;
+    let working_path = working_dir.path();
+
+    cli_run(working_path, backup_path)?
+        .args(["init", "filesystem"])
+        .assert()
+        .success();
+
+    // Let's back up these the source code.
+    cli_run(working_path, backup_path)?
+        .arg("backup")
+        .arg(project_dir.join("src"))
+        .assert()
+        .success();
+
+    // And make a copy of it.
+    let copy_dir = tempdir()?;
+    let copy_path = copy_dir.path();
+
+    cli_run(working_path, copy_path)?
+        .args(["init", "filesystem"])
+        .assert()
+        .success();
+
+    cli_run(working_path, backup_path)?
+        .args(["copy", "--skip", "backend", "--all", "--to"])
+        .arg(copy_path)
+        .assert()
+        .success();
+
+    // It didn't screw anything up, did it?
+    cli_run(working_path, backup_path)?
+        .args(["check", "-r"])
+        .assert()
+        .success();
+    cli_run(working_path, copy_path)?
+        .args(["check", "-r"])
+        .assert()
+        .success();
+
+    // Files should match except what we filtereed
+    let orig_files = cli_run(working_path, backup_path)?
+        .args(["ls", "LAST"])
+        .assert()
+        .success();
+    let orig_files_sans_filtered: Vec<&str> = stdout(&orig_files)
+        .trim()
+        .lines()
+        .filter(|l| !l.contains("backend"))
+        .collect();
+
+    let filtered_files = cli_run(working_path, copy_path)?
+        .args(["ls", "LAST"])
+        .assert()
+        .success();
+    let filtered_files: Vec<&str> = stdout(&filtered_files).trim().lines().collect();
+    assert_eq!(orig_files_sans_filtered, filtered_files);
+
+    // To examine results
+    // std::mem::forget(backup_dir);
+    // std::mem::forget(copy_dir);
+    Ok(())
+}
