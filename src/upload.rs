f57f4efce6bc905118906f7f5d0296dcfe878d16
Matt Kline <matt@bitbashing.io>
2024-09-28 00:13:45 -0700

    Thread up/down byte counts into CachedBackend

--- src/upload.rs
+++ src/upload.rs
@@ -2,7 +2,7 @@
 //! [snapshots](crate::snapshot)) to a [backend]
 
 use std::fs::File;
-use std::sync::{atomic::AtomicU64, mpsc::Receiver};
+use std::sync::mpsc::Receiver;
 
 use anyhow::Result;
 
@@ -17,11 +17,10 @@ pub fn upload(
     mode: Mode,
     cached_backend: &backend::CachedBackend,
     rx: Receiver<(String, File)>,
-    upload_byte_count: &AtomicU64,
 ) -> Result<()> {
     while let Ok((path, fh)) = rx.recv() {
         match mode {
-            Mode::LiveFire => cached_backend.write(&path, fh, upload_byte_count)?,
+            Mode::LiveFire => cached_backend.write(&path, fh)?,
             Mode::DryRun => {
                 // Just axe it, it isn't going anywhere.
                 drop(fh);
