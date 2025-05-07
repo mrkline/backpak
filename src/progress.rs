ef98d1328110bb26222c9d7e6db0edc0d37c0b93
Matt Kline <matt@bitbashing.io>
2025-05-06 23:53:55 -0700

    progress: Don't underflow
    Progress messages still get messed up if the terminal is too narrow
    and things that should take one line take several. How much do we care?

--- src/progress.rs
+++ src/progress.rs
@@ -191,6 +191,10 @@ pub fn print_download_line(downloaded_bytes: u64) {
 pub fn truncate_path(p: &Utf8Path, term: &Term) -> String {
     // Arbitrary truncation; do something smarter?
     let w = term.size().1 as usize; // (h, w) wut
+    // How absurd. But do it so we don't panic on underflow
+    if w <= 3 {
+        return ".".repeat(w)
+    }
     let syms: Vec<_> = p.as_str().graphemes(true).collect();
     if syms.len() > w {
         // If the filename itself is more than the line length (minus "..."),
